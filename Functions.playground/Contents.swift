cdff3e851a706ed7b9a053e610546683f9bba757
Chris Rehagen <42623969+ccrwy5@users.noreply.github.com>
2019-02-04 23:01:05 -0600

    Uploaded Functions.playground

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func check(value: Int) -> String? {
+    
+    if(value < 1){
+        return nil
+    }
+    
+    else{
+        return "\(value)"
+    }
+}
+
+let numberString1: String? = check(value: -2) 
+
+
+
+
+
